d098bc56ebb2079488f047416c1e88e525993024
Sieep-Coding <nstamb6@gmail.com>
2024-06-05 00:39:13 -0400

    added init

--- git.go
+++ git.go
@@ -1,7 +1,31 @@
 package main
 
-type Reposityory struct {
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type Repository struct {
 	RootDir string
 }
 
-type Commit struct
+type Commit struct {
+	Hash       string
+	Message    string
+	Author     string
+	Timestampe time.Time
+}
+
+func (repo *Repository) Init() {
+	gitDir := filepath.Join(repo.RootDir, ".git")
+	err := os.MkdirAll(gitDir, 0755)
+	if err != nil {
+		fmt.Println("Failed to initialize repository", err)
+		os.Exit(0)
+	}
+	fmt.Println("Initialized repository in:", gitDir)
+}
+
+func (repo )
